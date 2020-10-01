58fd557a7ba47a0862097ebcf80e33fdd9916186
BrunoGomesAluno <brunogomes@aluno.uema.br>
2020-10-01 18:06:56 -0300

    Lab 01 - Spring Initializr feito

---
+++ lab-01/springInitializr/fundamentos/src/test/java/edu/pw/fundamentos/FundamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pw.fundamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
