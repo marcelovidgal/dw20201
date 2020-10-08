32928c2731789fd2cb88c7911e8c3456b604a9f2
Matheus Adler <email@example.com>
2020-10-08 16:15:14 -0300

    Commit Restful Service Matheus

---
+++ apiconsumer/src/test/java/webserver/apiconsumer/ApiconsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package webserver.apiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiconsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
